e9261b3dac96668d8ca6c66d7107b9db09f69802
观澜听雨 <tangxiaochuane@yeah.net>
2024-12-29 20:08:17 +0800

    ## Setup -  create a expo router app -  setup the theme -  helpers - screen wrapper

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
+import Reacct from "react";
 
-export default function Index() {
+interface indexProps {}
+function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>index</Text>
     </View>
   );
 }
+
+export default index;
---
+++ components/ScreenWrapper.tsx
@@ -0,0 +1,28 @@
+import { View, Text } from "react-native";
+import Reacct from "react";
+import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface ScreenWrapperProps {
+  children: React.ReactNode;
+  bg: string;
+}
+function ScreenWrapper({ children, bg }: ScreenWrapperProps) {
+  const { top } = useSafeAreaInsets();
+
+  const paddingTop = top > 0 ? top + 5 : 30;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop,
+        backgroundColor: bg,
+      }}
+    >
+      {children}
+    </View>
+  );
+}
+
+export default ScreenWrapper;
